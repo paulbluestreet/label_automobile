83d7b22bf0b73af8ae8a401e5f72e6814d6685b7
Jurgen Buisman <imperatus@gmail.com>
2018-07-26 12:38:05 +0200

    Added random documentation

--- label_automobile/services/user.py
+++ label_automobile/services/user.py
@@ -6,4 +6,9 @@ class UserService:
         self._repository = UserRepository(session)
 
     def find_by_email(self, email):
+        """
+        Find a user by email address
+        :param email: str a user's email address
+        :return: User
+        """
         return self._repository.find_by_email(email)
